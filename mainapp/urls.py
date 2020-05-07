ff76030d2ff0defa87899ab04deec70951b06276
Cerzon <kirill.tkachenko@gmail.com>
2020-05-07 13:13:16 +0300

    lesson01 hw

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,7 @@ from mainapp import views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', mainapp.MainView.as_view(), name='index'),
+    path('', mainapp.index, name='index'),
+    path('products/', mainapp.products, name='products'),
+    path('contacts/', mainapp.contacts, name='contacts'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+# from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-class MainView(ListView):
-    pass
+# class IndexView(ListView):
+#     template_name = 'mainapp/index.html'
+
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
